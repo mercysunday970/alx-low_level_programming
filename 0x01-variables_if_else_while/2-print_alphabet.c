e09e4539df186a207fd452c8c5b5b9ae5cdfef1d
mercysunday970 <mirandaeswift686@gmail.com>
2023-04-21 09:23:26 -0700

    Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,10 @@ int main(void)
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 26; i++) 
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
 }
-	
