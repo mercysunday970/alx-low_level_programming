74773866b0059f8998b281720fd3910a0cb9f43d
mercysunday970 <mirandaeswift686@gmail.com>
2023-04-23 12:37:12 -0700

    nimbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,11 @@
  */
 int main(void)
 {
-	char num[10] = "0123456789";
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		putchar(num[i]);
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
