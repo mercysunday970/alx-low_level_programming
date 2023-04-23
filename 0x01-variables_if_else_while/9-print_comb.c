24c2515d0f7c5666e937d09197f8711f2b930e7e
mercysunday970 <mirandaeswift686@gmail.com>
2023-04-23 13:27:04 -0700

    Combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Single digits 
+ *
+ * Return: Always 0 (Successs)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
