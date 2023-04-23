283b960094b370500428c0122d4fc34a6e309878
mercysunday970 <mirandaeswift686@gmail.com>
2023-04-23 14:56:49 -0700

    Combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int n, m, i;
 
-	for (n = 48; n <= 58; n++)
+	for (n = 48; n < 58; n++)
 	{
-		for (m = 49; m <= 58; m++)
+		for (m = 49; m < 58; m++)
 		{
-			for (i = 50; i <= 58; i++)
+			for (i = 50; i < 58; i++)
 			{
 				if (i > m && m > n)
 				{
