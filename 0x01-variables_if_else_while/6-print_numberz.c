c761f96f2bc8dbc7505894058466368b1382e0eb
mercysunday970 <mirandaeswift686@gmail.com>
2023-04-23 12:26:23 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print all single numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
