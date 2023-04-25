53ef98d0027b70babe7946871674b6ba1a425fb7
mercysunday970 <mirandaeswift686@gmail.com>
2023-04-25 15:09:15 -0700

    Lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * main - Entry point
+ *
+ * Print all alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
