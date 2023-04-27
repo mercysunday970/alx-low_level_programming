5a2dce3f6ab20c253de38c36135aab9ee03eaf2a
mercysunday970 <mirandaeswift686@gmail.com>
2023-04-27 06:26:28 -0700

    Positive or negative

---
+++ 0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int;
+
+	printf("_putchar");
+	printf('\n');
+	return (0);
+}
