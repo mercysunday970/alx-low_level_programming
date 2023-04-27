66d0af8ff88545a5c66b6383dd562f1b838bd663
mercysunday970 <mirandaeswift686@gmail.com>
2023-04-26 18:29:11 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 	int i = 0;
-	long j = 1, z = 3;
+	long j = 1, k = 2;
 
 	while (i < 50)
 	{
 	if (i == 0)
 	printf("%ld", j);
-	else if (i = 1)
+	else if (i == 1)
 	printf (", %ld", k);
 	else
 	{
@@ -25,6 +25,6 @@ int main(void)
 	}
 	++i;
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
