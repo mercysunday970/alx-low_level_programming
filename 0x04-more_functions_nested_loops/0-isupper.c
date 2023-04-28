ca456c97087b8ae5e16893cfbe16f3265d516ea1
mercysunday970 <mirandaeswift686@gmail.com>
2023-04-28 07:03:31 -0700

    Upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper: prints uppercase characters
+ *
+ * @c: the charater to print
+ *
+ * Return: On success, 1 
+ * else print 0
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
