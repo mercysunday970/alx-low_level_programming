41e015e8dfc270c41568c944def2f6c9c6c5620e
mercysunday970 <mirandaeswift686@gmail.com>
2023-04-28 07:56:07 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h.>
+#include <stdio.h>
 
 /**
  * print_number - prints number 0-9
@@ -11,8 +11,6 @@ void print_numbers(void)
 	int c;
 
 	if (c = 0; c < 10; c++)
-	{
-		_putchar(c + 0);
-	}
+		_putchar(c + '0');
 	_putchar('\n');
 }
