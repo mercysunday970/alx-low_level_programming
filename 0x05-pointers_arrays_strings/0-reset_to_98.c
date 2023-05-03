0fceb810995259bbe917c014de521ca2ce06ed63
mercysunday970 <mirandaeswift686@gmail.com>
2023-05-03 05:06:18 -0700

    Reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - resets the values of an integer to 98
+ * @*n: the integer to be reset
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	int i;
+	n = i;
+
+	*n = 98;
+	printf("Value of i is: %d\n", i);
