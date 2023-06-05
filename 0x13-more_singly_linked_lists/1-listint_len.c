6600c349670beeacbe6c541dee8ae342cafc3026
mercysunday970 <mirandaeswift686@gmail.com>
2023-06-05 02:06:29 -0700

    length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints the
+ * number of elements in  linked list
+ * @h: head of list
+ *
+ * Retuurn: the number of elements
+ * in a liked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return(nodes);
+}
