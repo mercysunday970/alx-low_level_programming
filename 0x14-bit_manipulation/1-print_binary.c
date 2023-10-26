104c7f5754c19c1b515f886b2e2912fb584de011
mercysunday970 <mirandaeswift686@gmail.com>
2023-10-26 05:47:35 -0700

    else statement

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,11 +7,18 @@
  * of a number
  * @n: integer to convert
  *
- * Return: On success 0
+ * Return: no return
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	_putchar(( n & 1) + '0');
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
