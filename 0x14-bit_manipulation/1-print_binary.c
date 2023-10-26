b7a75bdf325670dc6ef84ca612622f8d9e2b643b
mercysunday970 <mirandaeswift686@gmail.com>
2023-10-26 05:34:34 -0700

    a very small change

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,11 +6,12 @@
  * print_binary - prints the binary representation
  * of a number
  * @n: integer to convert
+ *
+ * Return: On success 0
  */
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
-
 	_putchar(( n & 1) + '0');
 }
