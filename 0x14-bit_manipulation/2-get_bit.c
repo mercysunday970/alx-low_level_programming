9cd76a351135c46b641f398196c499767fa3020d
mercysunday970 <mirandaeswift686@gmail.com>
2023-10-26 05:56:21 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,16 +7,22 @@
  * @index: is the index
  * @n: the bit
  * 
- * Return:the value of the int at the index
+ * Return:the value of the bit at the index
  * if error, -1
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
-		return (-1);
+	unsigned int i;
 
-	if ((n & (1 << index)) == 0)
+	if (n == 0 && index < 64)
 		return (0);
 
-	return (1);
+	for (i == 0; i <= 63; n >>= 1; i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+	return (-1);
 }
