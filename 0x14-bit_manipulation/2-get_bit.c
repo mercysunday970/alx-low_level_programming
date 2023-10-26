5efd2ce1b3c35a216a197dd40bc7612d70b43c0f
mercysunday970 <mirandaeswift686@gmail.com>
2023-10-26 05:59:07 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -17,12 +17,13 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (n == 0 && index < 64)
 		return (0);
 
-	for (i == 0; i <= 63; n >>= 1; i++)
+	for (i = 0; i <= 63; n >>= 1, i++)
 	{
 		if (index == i)
 		{
 			return (n & 1);
 		}
 	}
+
 	return (-1);
 }
